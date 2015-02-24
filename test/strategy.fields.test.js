2aa1a33d3bc68b695693de8b0611c1d4666c6cb0
Michael Bell <mbell8903@yahoo.com>
2015-02-24 11:42:09 -0500

    Initial commit

---
+++ test/strategy.fields.test.js
@@ -0,0 +1,83 @@
+/* global describe, it, expect, before */
+/* jshint expr: true */
+
+var chai = require('chai')
+  , Strategy = require('../lib/strategy');
+
+
+describe('Strategy', function() {
+
+  describe('handling a request with valid credentials in body using custom field name', function() {
+    var strategy = new Strategy({ tokenFields: ['tok'] }, function(token, done) {
+      if (token == 'abcdefghijklmnopqrstuvwxyz') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+
+    var user
+      , info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.tok = 'abcdefghijklmnopqrstuvwxyz';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
+  describe('handling a request with valid credentials in body using custom field name with object notation', function() {
+    var strategy = new Strategy({ tokenFields: 'user[token]' }, function(token, done) {
+      if (token == 'abcdefghijklmnopqrstuvwxyz') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+
+    var user
+      , info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.user = {};
+          req.body.user.token = 'abcdefghijklmnopqrstuvwxyz';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
+});
---
+++ test/strategy.normal.test.js
@@ -0,0 +1,160 @@
+/* global describe, it, expect, before */
+/* jshint expr: true */
+
+var chai = require('chai')
+  , Strategy = require('../lib/strategy');
+
+
+describe('Strategy', function() {
+
+  describe('handling a request with valid credentials in body', function() {
+    var strategy = new Strategy(function(token, done) {
+      if (token == 'abcdefghijklmnopqrstuvwxyz') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+
+    var user
+      , info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.token = 'abcdefghijklmnopqrstuvwxyz';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
+  describe('handling a request with valid credentials in query', function() {
+    var strategy = new Strategy(function(token, done) {
+      if (token == 'abcdefghijklmnopqrstuvwxyz') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+
+    var user
+      , info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.query.token = 'abcdefghijklmnopqrstuvwxyz';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
+  describe('handling a request without a body', function() {
+    var strategy = new Strategy(function(token, done) {
+      throw new Error('should not be called');
+    });
+
+    var info, status;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .fail(function(i, s) {
+          info = i;
+          status = s;
+          done();
+        })
+        .authenticate();
+    });
+
+    it('should fail with info and status', function() {
+      expect(info).to.be.an.object;
+      expect(info.message).to.equal('Missing auth token');
+      expect(status).to.equal(400);
+    });
+  });
+
+  describe('handling a request without a body, but no token', function() {
+    var strategy = new Strategy(function(token, done) {
+      throw new Error('should not be called');
+    });
+
+    var info, status;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .fail(function(i, s) {
+          info = i;
+          status = s;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+        })
+        .authenticate();
+    });
+
+    it('should fail with info and status', function() {
+      expect(info).to.be.an.object;
+      expect(info.message).to.equal('Missing auth token');
+      expect(status).to.equal(400);
+    });
+  });
+
+  describe('handling a request without a body, but no token', function() {
+    var strategy = new Strategy(function(token, done) {
+      throw new Error('should not be called');
+    });
+
+    var info, status;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .fail(function(i, s) {
+          info = i;
+          status = s;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+        })
+        .authenticate();
+    });
+
+    it('should fail with info and status', function() {
+      expect(info).to.be.an.object;
+      expect(info.message).to.equal('Missing auth token');
+      expect(status).to.equal(400);
+    });
+  });
+
+});
